668722b1d7abda493b52888bb00437ea5ab454f6
Bhavik Jain <bhavikjain358@gmail.com>
2021-01-28 11:08:47 -0500

    COVID19: Dockerized backend

--- service-covid19-tracker/service-covid19-tracker-backend/build.sh
+++ service-covid19-tracker/service-covid19-tracker-backend/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./gradlew clean build
 docker build --build-arg JAR_FILE=build/libs/*.jar -t bhavik-services-covid19-backend .
-docker tag bhavik-services-covid19-backend docker-registry:5000/bhavik-services-covid19-backend
-docker push docker-registry:5000/bhavik-services-covid19-backend
+docker tag bhavik-services-covid19-backend bhavik3jain/bhavik-services-covid19-backend:latest
+docker push bhavik3jain/bhavik-services-covid19-backend:latest
