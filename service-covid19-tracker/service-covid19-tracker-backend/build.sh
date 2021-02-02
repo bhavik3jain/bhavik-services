ae1046d4a277036a87098c1bde058312de142814
Bhavik Jain <bhavikjain358@gmail.com>
2021-02-02 09:57:19 -0500

    COVID19: uprev

--- service-covid19-tracker/service-covid19-tracker-backend/build.sh
+++ service-covid19-tracker/service-covid19-tracker-backend/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./gradlew clean build
 docker build --build-arg JAR_FILE=build/libs/*.jar -t bhavik-services-covid19-backend .
-docker tag bhavik-services-covid19-backend bhavik3jain/bhavik-services-covid19-backend:1.6
-docker push bhavik3jain/bhavik-services-covid19-backend:1.6
+docker tag bhavik-services-covid19-backend bhavik3jain/bhavik-services-covid19-backend:1.7
+docker push bhavik3jain/bhavik-services-covid19-backend:1.7
