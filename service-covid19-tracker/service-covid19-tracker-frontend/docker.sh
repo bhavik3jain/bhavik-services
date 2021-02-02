968798b5b8a58ae91a6d77ef97589cde89eb23f2
Bhavik Jain <bhavikjain358@gmail.com>
2021-02-02 12:28:02 -0500

    COVID19: FE changes

--- service-covid19-tracker/service-covid19-tracker-frontend/docker.sh
+++ service-covid19-tracker/service-covid19-tracker-frontend/docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t bhavik-services-covid19-frontend .
-docker tag bhavik-services-covid19-frontend bhavik3jain/bhavik-services-covid19-frontend:1.7
-docker push bhavik3jain/bhavik-services-covid19-frontend:1.7
+docker tag bhavik-services-covid19-frontend bhavik3jain/bhavik-services-covid19-frontend:1.8
+docker push bhavik3jain/bhavik-services-covid19-frontend:1.8
